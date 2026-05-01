5ff07e4fca69c4f1777846a344887ec8cfc5ded5
Erick <eribuit9@gmail.com>
2026-04-30 22:02:30 -0500

    feat(erick): creación del UserCreationService.java

---
+++ src/main/java/CinePacho/demo/shared/enumeration/user/UserCreationService.java
@@ -0,0 +1,33 @@
+package CinePacho.demo.shared.enumeration.user;
+
+import CinePacho.demo.auth.entities.UserEntity;
+import CinePacho.demo.auth.repository.UserRepository;
+import CinePacho.demo.shared.enumeration.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCreationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserCreationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserEntity createUser(String name, String password, UserType userType) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(name);
+        userEntity.setPassword(passwordEncoder.encode(password));
+        userEntity.setUserType(userType);
+
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity findUserByUsername(String username) {
+        return userRepository.findUserEntityByUsername(username);
+    }
+}
