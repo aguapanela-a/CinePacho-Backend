93c80d644005df3701b9aa3f67bc1c7ddeb859e0
Erick <eribuit9@gmail.com>
2026-04-30 21:38:08 -0500

    feat(erick): implementar estructura base del dominio de auth
    - agregar entidad UserEntity
    - agregar repositorio UserRepository para persistencia
    - agregar enum UserType para clasificación de tipos de usuarios

---
+++ src/main/java/CinePacho/demo/auth/entities/UserEntity.java
@@ -0,0 +1,30 @@
+package CinePacho.demo.auth.entities;
+
+import CinePacho.demo.shared.enumeration.UserType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter @Getter
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    UserType userType;
+
+}
---
+++ src/main/java/CinePacho/demo/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package CinePacho.demo.auth.repository;
+
+
+import CinePacho.demo.auth.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+}
