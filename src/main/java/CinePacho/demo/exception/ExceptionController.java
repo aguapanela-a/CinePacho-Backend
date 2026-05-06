37732a251454ea847f67b8ff985f2ec5bb67831a
Erick <eribuit9@gmail.com>
2026-05-05 20:15:31 -0500

    Excepción global para capturar userType de entrada inválido | Erick

--- src/main/java/CinePacho/demo/exception/ExceptionController.java
+++ src/main/java/CinePacho/demo/exception/ExceptionController.java
@@ -2,9 +2,11 @@ package CinePacho.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
+import tools.jackson.databind.exc.InvalidTypeIdException;
 
 @RestControllerAdvice
 
@@ -22,6 +24,13 @@ public class ExceptionController {
     public ResponseEntity<ErrorDTO> cinePachoException(HttpClientErrorException.Forbidden ex){
         ErrorDTO error = new ErrorDTO(HttpStatus.FORBIDDEN.value(), "La solicitud falló por permisos insuficientes: " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
+
+    //Validar tipos de usuario de entrada al registro
+    @ExceptionHandler(InvalidTypeIdException.class)
+    public ResponseEntity<ErrorDTO> handleInvalidUserType(InvalidTypeIdException ex) {
+       ErrorDTO error = new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "Tipo de usuario no válido para auto-registro. " + ex.getMessage());
+       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 
