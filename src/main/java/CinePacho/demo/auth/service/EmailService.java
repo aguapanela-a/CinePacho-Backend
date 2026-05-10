1f0c6e2ea6e3c204d5e50af259e4129a0af79f49
Erick <eribuit9@gmail.com>
2026-05-10 06:58:45 -0500

    Migración al servicio Resend para envío de correos en deploy

--- src/main/java/CinePacho/demo/auth/service/EmailService.java
+++ src/main/java/CinePacho/demo/auth/service/EmailService.java
@@ -1,7 +1,9 @@
 package CinePacho.demo.auth.service;
 
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import com.resend.Resend;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +19,29 @@ public class EmailService {
     @Value("${app.base-url}")
     private String baseUrl;
 
-    public void sendVerificationEmail(UserEntity user, String token) {
+    @Value("${app.resend-API-key}")
+    private String resendKey;
+
+    public void sendVerificationEmail(UserEntity user, String token) throws ResendException {
         String link = baseUrl + "/api/auth/verify?token=" + token;
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(user.getEmail());
-        message.setSubject("Confirma tu cuenta");
-        message.setText(
-            "Hola " + user.getUsername() + ",\n\n" +
-            "Haz clic en el siguiente enlace para confirmar tu cuenta:\n" +
-            link + "\n\n" +
-            "El enlace expira en 24 horas.\n\n" +
-            "Si no creaste esta cuenta, ignora este mensaje."
-        );
+        String message = "Hola " + user.getUsername() + ",\n\n" +
+                "Haz clic en el siguiente enlace para confirmar tu cuenta:\n" +
+                link + "\n\n" +
+                "El enlace expira en 24 horas.\n\n" +
+                "Si no creaste esta cuenta, ignora este mensaje.";
 
-        mailSender.send(message);
+        Resend resend = new Resend(resendKey);
+
+        CreateEmailOptions emailOptions = CreateEmailOptions.builder()
+                        .from("\"CinePacho <onboarding@resend.dev>\"")
+                                .to(user.getEmail())
+                                        .subject("Confirma tu cuenta")
+                                                .text(message)
+                                                        .build();
+
+        resend.emails().send(emailOptions);
     }
+
+
 }
